2f4448f01d58d8498d76600d7fbb4b354e6bb521
vitoriajancollab <vitoriajandreixe85@gmail.com>
2026-09-21 23:06:18 -0300

    sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { createClient } from '@libsql/client';
-import { NextResponse } from 'next/server';
+
 
 const client = createClient({
   url: process.env.TURSO_DATABASE_URL,
